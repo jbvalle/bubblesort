95d00c37f7a51ea1fcc4c1b0a6670e8e3a53686e
John Bryan Valle <jbvalle.jv@gmail.com>
2021-10-26 19:11:04 +0200

    optimized and finalzed randomizer algorithm

--- src/main.c
+++ src/main.c
@@ -24,18 +24,19 @@ int main(void)
     scanf("%d", &sizeof_array);getchar();
 
     random_array = (int*)malloc(abs_rand_range*sizeof(int));
+
     random_array = randomizer(abs_rand_range);
 
     printf("\nRandom Field: \n");
     show_random_array(random_array,sizeof_array);
 
     printf("\nBubble Sorted Increasing: \n");
-    int* bubble_sort = bubblesort_increasing(random_array, 20);
-    show_random_array(bubble_sort,20);
+    int* bubble_sort = bubblesort_increasing(random_array, sizeof_array);
+    show_random_array(bubble_sort,sizeof_array);
 
     printf("\nBubble Sorted Decreasing: \n");
-    int* bubble_sort_reverse = bubblesort_decreasing(random_array, 20);
-    show_random_array(bubble_sort_reverse,20);
+    int* bubble_sort_reverse = bubblesort_decreasing(random_array, sizeof_array);
+    show_random_array(bubble_sort_reverse,sizeof_array);
     printf("\n");
 
     return 0;
--- src/randomizer.c
+++ src/randomizer.c
@@ -4,41 +4,26 @@
 
 int *randomizer(int abs_rand_range) {
 
-  int matching_flag;
+  int x;
   int* random_array;
 
   random_array = (int*)malloc(abs_rand_range*sizeof(int));
 
-  //Initializing random_array with NULL
-  for (int i = 0; i < abs_rand_range; i++) random_array[i]= 0;
+  srand(time(NULL));
 
-  //Assigning numbers 1 to abs_rand_range to random memory Locations
-  for(int number = 1; (matching_flag != 1); number++){
+  for (int i = 0; i < abs_rand_range;) {
 
-      //Generating Random number from 0 to abs_rand_range
-      srand(clock());
-      int memory_location = (rand() % abs_rand_range);
+    int r=rand()%abs_rand_range+1;
 
-      //Analyzing free random memory location
-      for (int i = 1; (random_array[memory_location] != 0)&&(i < abs_rand_range); i++) {
-        memory_location = (memory_location + 1) % abs_rand_range;
-      }
-      random_array[memory_location] = number;
-
-
-      matching_flag = 0;
-      //Checking for duplicates in random_array
-      //Break if all memory location have been assigned
-      for (int h = 0; h < abs_rand_range; h++){
-
-        if(random_array[h] == 0){
-          matching_flag = 0;
-          break;
-        }else {
-          matching_flag = 1;
+    for (x = 0; x < i; x++)
+    {
+        if(random_array[x]==r){
+            break;
         }
-      }
-
+    }
+    if(x==i){
+        random_array[i++]=r;
+    }
   }
 
   return random_array;
